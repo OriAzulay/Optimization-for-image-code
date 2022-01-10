3702001807661018073c96f000fd884c4a55d7aa
oriAZul <ori101914@gmail.com>
2022-01-10 22:16:06 +0200

    Change my function and merge all the help methods

--- myfunction.c
+++ myfunction.c
@@ -1,4 +1,12 @@
 #include <stdbool.h> 
+// DEFINE CONSTS THAT USED AT 'applypixel' and used in the file
+#define KERNEL_SIZE 3
+#define MIN_INTENSITY 766
+#define MAX_INTENSITY -1
+// define function here make the access faster
+#define MIN(a,b) ((a<b ? a:b))
+#define MAX(a,b) ((a>b ? a:b))
+#define CALC_INDEX(i,j,n) ((i)*(j) + (j))
 
 typedef struct {
    unsigned char red;
@@ -146,6 +154,7 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
 	}
 }
 
+//got replaced by memcpy
 void charsToPixels(Image *charsImg, pixel* pixels) {
 
 	int row, col;
@@ -158,6 +167,7 @@ void charsToPixels(Image *charsImg, pixel* pixels) {
 		}
 	}
 }
+
 
 void pixelsToChars(pixel* pixels, Image *charsImg) {
 
@@ -172,6 +182,7 @@ void pixelsToChars(pixel* pixels, Image *charsImg) {
 	}
 }
 
+//got replaced by memcpy
 void copyPixels(pixel* src, pixel* dst) {
 
 	int row, col;
@@ -187,11 +198,17 @@ void copyPixels(pixel* src, pixel* dst) {
 
 void doConvolution(Image *image, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale, bool filter) {
 
-	pixel* pixelsImg = malloc(m*n*sizeof(pixel));
-	pixel* backupOrg = malloc(m*n*sizeof(pixel));
+    // instead multiplying n*m*size of we'll do one calculation
+    int size = m*n*sizeof(pixel);
+	pixel* pixelsImg = malloc(size);
+	pixel* backupOrg = malloc(size);
 
-	charsToPixels(image, pixelsImg);
-	copyPixels(pixelsImg, backupOrg);
+	//instead of calling a function, we can use memcpy:
+	
+	memcpy(pixelsImg, image->data, size);//copyng 'size' byts from image->data to pixelsImg
+	//charsToPixels(image, pixelsImg);
+	memcpy(backupOrg, pixelsImg,size);
+	//copyPixels(pixelsImg, backupOrg);
 
 	smooth(m, backupOrg, pixelsImg, kernelSize, kernel, kernelScale, filter);
 
@@ -201,34 +218,201 @@ void doConvolution(Image *image, int kernelSize, int kernel[kernelSize][kernelSi
 	free(backupOrg);
 }
 
+// Writting all the function at this scope make the code faster
 void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName, char* filteredBlurRsltImgName, char* filteredSharpRsltImgName, char flag) {
+	// instead multiplying n*m*size of we'll do one calculation
+    int size = m*n*sizeof(pixel);
+	pixel* pixelsImg = malloc(size);
+	pixel* backupOrg = malloc(size);
 
+	// Register members can be more accessiables
+	register int i,j; //members for conditions loop
+	register int m_kernel = m-1; // right corner border
+	register pixel p;
+	register pixel current_pixel;
+	register int red_sum, green_sum, blue_sum;
+	register int in_place; // integer for current index
+
+	if (flag == '1') {	
+	//instead of calling a function, we can use memcpy:
+	memcpy(pixelsImg, image->data, size);//copyng 'size' byts from image->data to pixelsImg
+	//charsToPixels(image, pixelsImg);
+	memcpy(backupOrg, pixelsImg,size);
+	//copyPixels(pixelsImg, backupOrg);
+	i = m_kernel;
+	/**
+	 * loop stop when i==0
+	 * while loop more efficient then for
+	 * this is represent the 'doConvolution' func
+	**/
+	
+	/**
+	#blur image:
 	/*
 	* [1, 1, 1]
 	* [1, 1, 1]
 	* [1, 1, 1]
 	*/
-	int blurKernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
-
+	//int blurKernel[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+	/** to create matrix and insert in function may take time so 
+	 * all the calculation will be imidiatlly.
+	**/
+	while(--i){
+		j=m_kernel;
+		while (--j){
+			/**
+			 * this part is like smooth inside convolution
+			 * and the 'applyKernel' func
+			**/
+			red_sum = green_sum = blue_sum = 0;
+			in_place = (i-1)*m;
+			in_place += j-1;
+			// applyKernel at in_place:
+			// backup0rg[0,0]
+			p = backupOrg[in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[0,1]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[0,2]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[1,0]
+			in_place += (m-2);
+			p = backupOrg[in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[1,1]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[1,2]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[2,0]
+			in_place += (m-2);			
+			p = backupOrg[in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[2,1]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// backup0rg[2,2]
+			p = backupOrg[++in_place];
+			red_sum += p.red;
+			green_sum += p.green;
+			blue_sum += p.blue;
+			// now assign result pixel at [i][j]
+			// and find if there's max/min
+			current_pixel.red = (unsigned char)(MIN(MAX(red_sum/9,0),255));
+			current_pixel.blue = (unsigned char)(MIN(MAX(blue_sum/9,0),255));
+			current_pixel.green = (unsigned char)(MIN(MAX(green_sum/9,0),255));
+			pixelsImg[CALC_INDEX(i,j,m)] = current_pixel;
+		}
+	}
+	//pixelToChar(backupOrg,pixelsImg);
+	memcpy(image->data,pixelsImg,size);
+	// write result image to file
+	writeBMP(image, srcImgpName, blurRsltImgName);	
+	//doConvolution(image, 3, blurKernel, 9, false);
+	//**********//
+	/**
+	#Sharp image:
 	/*
 	* [-1, -1, -1]
 	* [-1, 9, -1]
 	* [-1, -1, -1]
 	*/
-	int sharpKernel[3][3] = {{-1,-1,-1},{-1,9,-1},{-1,-1,-1}};
+	//int sharpKernel[3][3] = {{-1, -1, -1}, {-1, 9, -1}, {-1, -1, -1}};
+	/** to create matrix and insert in function may take time so 
+	 * all the calculation will be imidiatlly.
+	**/
+	//same as blur:
+	memcpy(pixelsImg, image->data, size);
+	memcpy(backupOrg, pixelsImg,size);
+	i = m_kernel;
+	while(--i){
+		j=m_kernel;
+		while (--j){
+			// now the initialation is -1 for sharpen
+			red_sum = green_sum = blue_sum = 0;
+			in_place = (i-1)*m;
+			in_place += j-1;
+			// applyKernel at in_place:
+			// backup0rg[0,0]
+			p = backupOrg[in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[0,1]
+			p = backupOrg[++in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[0,2]
+			p = backupOrg[++in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[1,0]
+			in_place += (m-2);
+			p = backupOrg[in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[1,1] for current pixel cell
+			p = backupOrg[++in_place];
+			red_sum += p.red * 9;
+			green_sum += p.green* 9;
+			blue_sum += p.blue * 9;
+			// backup0rg[1,2]
+			p = backupOrg[++in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[2,0]
+			in_place += (m-2);			
+			p = backupOrg[in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[2,1]
+			p = backupOrg[++in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// backup0rg[2,2]
+			p = backupOrg[++in_place];
+			red_sum -= p.red;
+			green_sum -= p.green;
+			blue_sum -= p.blue;
+			// now assign result pixel at [i][j]
+			// and find if there's max/min
+			current_pixel.red = (unsigned char)(MIN(MAX(red_sum,0),255));
+			current_pixel.blue = (unsigned char)(MIN(MAX(blue_sum,0),255));
+			current_pixel.green = (unsigned char)(MIN(MAX(green_sum,0),255));
+			pixelsImg[CALC_INDEX(i,j,m)] = current_pixel;
+		}
+	}
+	//pixelToChar(backupOrg,pixelsImg);
+	memcpy(image->data,pixelsImg,size);
+	// write result image to file
+	writeBMP(image, srcImgpName, sharpRsltImgName);	
+	//doConvolution(image, 3, sharpKernel, 1, false);	
 
-	if (flag == '1') {	
-		// blur image
-		doConvolution(image, 3, blurKernel, 9, false);
-
-		// write result image to file
-		writeBMP(image, srcImgpName, blurRsltImgName);	
-
-		// sharpen the resulting image
-		doConvolution(image, 3, sharpKernel, 1, false);
-		
-		// write result image to file
-		writeBMP(image, srcImgpName, sharpRsltImgName);	
 	} else {
 		// apply extermum filtered kernel to blur image
 		doConvolution(image, 3, blurKernel, 7, true);
